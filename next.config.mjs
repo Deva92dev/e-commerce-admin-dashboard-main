9a0bfc13a5ac0d40dd8420330c7245583b62684e
Deva92dev <buildwith92deva@gmail.com>
2024-12-11 12:14:43 +0530

    Changes  again in nextjs config

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  env: {
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+  },
   images: {
     remotePatterns: [
       {
@@ -25,9 +28,6 @@ const nextConfig = {
         ],
       },
     ];
-  },
-  experimental: {
-    appDir: true,
   },
 };
 
