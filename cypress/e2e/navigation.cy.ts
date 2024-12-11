3369d3bff2787e3d06501972d26f7bfdc22d3740
Deva92dev <buildwith92deva@gmail.com>
2024-12-11 11:44:57 +0530

    Removed cypress from completed projects

--- cypress/e2e/navigation.cy.ts
+++
@@ -1,9 +0,0 @@
-describe("Navigation Tests", () => {
-  it("should navigate to collections page", () => {
-    cy.viewport(1280, 720);
-    cy.visit("http://localhost:3000");
-    cy.get('a[href*="collections"]').click({ multiple: true });
-    cy.url().should("include", "/collections");
-    cy.get("h1").should("exist");
-  });
-});
